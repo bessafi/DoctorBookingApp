e9bca02d94596f1e425a0cd6b365a685c2012cf8
Shujaa <jospers557@gmail.com>
2024-12-26 17:28:47 -0500

    Refactor AuthExceptionHandler to return ApiResponse instead of Error for exception handling

--- src/main/java/shujaa/authentication_with_spring/security/controller/advice/AuthExceptionHandler.java
+++ src/main/java/shujaa/authentication_with_spring/security/controller/advice/AuthExceptionHandler.java
@@ -4,42 +4,48 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import shujaa.authentication_with_spring.security.controller.response.ApiResponse;
 
 @ControllerAdvice
 public class AuthExceptionHandler {
 
     @ExceptionHandler(AuthException.UserAlreadyExistsException.class)
-    public ResponseEntity<Error> handleUserAlreadyExistsException(AuthException.UserAlreadyExistsException ex) {
-        return new ResponseEntity<>(new Error(ex.getMessage()), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ApiResponse> handleUserAlreadyExistsException(AuthException.UserAlreadyExistsException ex) {
+        return buildResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(AuthException.UsernameAlreadyExistsException.class)
-    public ResponseEntity<Error> handleUsernameAlreadyExistsException(AuthException.UsernameAlreadyExistsException ex) {
-        return new ResponseEntity<>(new Error(ex.getMessage()), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ApiResponse> handleUsernameAlreadyExistsException(AuthException.UsernameAlreadyExistsException ex) {
+        return buildResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(AuthException.UserNotFoundException.class)
-    public ResponseEntity<Error> handleUserNotFoundException(AuthException.UserNotFoundException ex) {
-        return new ResponseEntity<>(new Error(ex.getMessage()), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ApiResponse> handleUserNotFoundException(AuthException.UserNotFoundException ex) {
+        return buildResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(AuthException.AccountNotVerifiedException.class)
-    public ResponseEntity<Error> handleAccountNotVerifiedException(AuthException.AccountNotVerifiedException ex) {
-        return new ResponseEntity<>(new Error(ex.getMessage()), HttpStatus.FORBIDDEN);
+    public ResponseEntity<ApiResponse> handleAccountNotVerifiedException(AuthException.AccountNotVerifiedException ex) {
+        return buildResponse(ex.getMessage(), HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(AuthException.VerificationCodeExpiredException.class)
-    public ResponseEntity<Error> handleVerificationCodeExpiredException(AuthException.VerificationCodeExpiredException ex) {
-        return new ResponseEntity<>(new Error(ex.getMessage()), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ApiResponse> handleVerificationCodeExpiredException(AuthException.VerificationCodeExpiredException ex) {
+        return buildResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(AuthException.InvalidVerificationCodeException.class)
-    public ResponseEntity<Error> handleInvalidVerificationCodeException(AuthException.InvalidVerificationCodeException ex) {
-        return new ResponseEntity<>(new Error(ex.getMessage()), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ApiResponse> handleInvalidVerificationCodeException(AuthException.InvalidVerificationCodeException ex) {
+        return buildResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Error> handleGlobalException(Exception ex) {
-        return new ResponseEntity<>(new Error("An unexpected error occurred: " + ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<ApiResponse> handleGlobalException(Exception ex) {
+        return buildResponse("An unexpected error occurred. Please try again later.", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private ResponseEntity<ApiResponse> buildResponse(String message, HttpStatus status) {
+        ApiResponse apiResponse = new ApiResponse(message);
+        return new ResponseEntity<>(apiResponse, status);
     }
 }
